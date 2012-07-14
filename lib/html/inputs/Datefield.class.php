0e4b5d0c7b6c94654bcfba75b44b9ebbf22d1a2a
Christian Ackermann <prdatur@gmail.com>
2012-07-14 19:46:58 +0200

    added Datefield input class

---
+++ lib/html/inputs/Datefield.class.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Provides a HTML-Textfield with a date picker
+ *
+ * @copyright Christian Ackermann (c) 2010 - End of life
+ * @author Christian Ackermann <prdatur@gmail.com>
+ * @package lib.html.inputs
+ * @category Form.Inputs
+ */
+class Datefield extends Textfield
+{
+
+	/**
+	 * constructor
+	 *
+	 * @param string $name 
+	 *   the input name
+	 * @param string $value 
+	 *   the value for this input (optional, default='')
+	 * @param string $label 
+	 *   the input label (optional, default='')
+	 * @param string $description 
+	 *   the input description (optional, default = '')
+	 * @param string $class 
+	 *   the input css class (optional, default = '')
+	 * @param string $id 
+	 *   the input id (optional, default = '')
+	 */
+	public function __construct($name, $value = '', $label = '', $description = '', $class = "", $id = '') {
+		parent::__construct($name, $value, $label, $description, $class, $id);
+		$this->config_array('css_class', 'datepicker');
+	}
+
+}
+
+?>
