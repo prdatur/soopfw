89d9ce25151ccb9f2cc1f8f70c90604c8e96ba90
Christian Ackermann <prdatur@gmail.com>
2012-08-25 22:57:29 +0200

    fix code style

--- modules/system/js/updatedb.js
+++ modules/system/js/updatedb.js
@@ -1,32 +1,32 @@
 Soopfw.behaviors.system_updater = function() {
-		if(Soopfw.config.update_db_modules == undefined) {
-			return;
-		}
-		var max_updates = Soopfw.config.update_db_modules.length;
-		var current = 1;
-		Soopfw.ajax_queue_init("system_updatedb");
+	if(Soopfw.config.update_db_modules == undefined) {
+		return;
+	}
+	var max_updates = Soopfw.config.update_db_modules.length;
+	var current = 1;
+	Soopfw.ajax_queue_init("system_updatedb");
 
-		Soopfw.progress("system_updatedb", max_updates, Soopfw.t("Updating Module 1 from icomplete", { 'icomplete': max_updates}), "#updatedb_status");
+	Soopfw.progress("system_updatedb", max_updates, Soopfw.t("Updating Module 1 from icomplete", { 'icomplete': max_updates}), "#updatedb_status");
 
-		for(var i = 0; i < Soopfw.config.update_db_modules.length; i++) {
+	for(var i = 0; i < Soopfw.config.update_db_modules.length; i++) {
 
-			var module = Soopfw.config.update_db_modules[i];
-			Soopfw.ajax_queue("system_updatedb",{
+		var module = Soopfw.config.update_db_modules[i];
+		Soopfw.ajax_queue("system_updatedb",{
 
-				url: "/system/update_module/" + module + "/js",
-				success: function(){
-					var module_updated = Soopfw.config.update_db_modules[current];
-					if(current >= max_updates) {
-						Soopfw.progress_update("system_updatedb", Soopfw.t("Updating Module completed", {'icurrent': current, 'icomplete': max_updates}));
-						location.href = "/admin/system/updatedb";
-						return;
-					}
-					current++;
-					Soopfw.progress_update("system_updatedb", Soopfw.t("Updating Module: @module (icurrent from icomplete)", {'@module': module_updated, 'icurrent': current, 'icomplete': max_updates}));
-
+			url: "/system/update_module/" + module + "/js",
+			success: function(){
+				var module_updated = Soopfw.config.update_db_modules[current];
+				if(current >= max_updates) {
+					Soopfw.progress_update("system_updatedb", Soopfw.t("Updating Module completed", {'icurrent': current, 'icomplete': max_updates}));
+					location.href = "/admin/system/updatedb";
+					return;
 				}
-			});
-		}
-		Soopfw.ajax_queue_start("system_updatedb");
+				current++;
+				Soopfw.progress_update("system_updatedb", Soopfw.t("Updating Module: @module (icurrent from icomplete)", {'@module': module_updated, 'icurrent': current, 'icomplete': max_updates}));
 
-	};
+			}
+		});
+	}
+	Soopfw.ajax_queue_start("system_updatedb");
+
+};
