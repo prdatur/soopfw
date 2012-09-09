e6aea91d94d9005cff6b643b8d20708f866c4d06
Christian Ackermann <prdatur@gmail.com>
2012-09-09 18:36:05 +0200

    add basic login check test

--- modules/system/tests/DefaultWebSystemTest.class.php
+++ modules/system/tests/DefaultWebSystemTest.class.php
@@ -27,7 +27,20 @@ class DefaultWebSystemTest extends WebUnitTest implements UnitTestInterface
 	 */
 	public function check_root() {
 		$this->do_get('/user/login.html');
-		$this->assert_web_regexp('/value="Login" name="soopfw_login"/', t('Validate user login form'));
+		$this->assert_true((preg_match("/type=\"submit\"\s+value=\"([^\"]+)\"\s+name=\"soopfw_login\"/", $this->content, $matches) == 1), t('Validate user login form'));
+		$this->do_post('/user/login.html', array(
+			'soopfw_login' => $matches[1],
+		), true);
+
+		$this->assert_web_regexp('/You have entered a wrong username and\/or password\./', t('Check invalid login'));
+
+		$this->do_post('/user/login.html', array(
+			'soopfw_login' => $matches[1],
+			'user' => 'admin_create',
+			'pass' => 'admin',
+		), true);
+
+		$this->assert_web_regexp('/>My account<\/a>/', t('Check valid login'));
 	}
 }
 ?>
