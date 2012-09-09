e632f5ce05506e4a4608cadfdc850daac5285867
Christian Ackermann <prdatur@gmail.com>
2012-09-09 13:13:11 +0200

    Create session identifier file to let the core recognize it as a WebUnitTest

--- lib/tests/WebUnitTest.class.php
+++ lib/tests/WebUnitTest.class.php
@@ -23,11 +23,41 @@ class WebUnitTest extends UnitTest {
 	 */
 	protected $client = null;
 
+	/**
+	 * The base url to do queries.
+	 * @var string
+	 */
+	private $base_domain = '';
+
+	/**
+	 * The session id for the client.
+	 *
+	 * @var string
+	 */
+	private $client_session_id = "";
+
 	public function __construct(&$core = null) {
 		parent::__construct($core);
+
+		$this->base_domain = 'http://' . $this->core->core_config('core', 'domain');
+
 		$this->client = new HttpClient();
+		$this->client->do_get($this->base_domain . '/');
+		if (preg_match("/" . preg_quote($this->core->core_config('core', 'domain'), '/') . "\t+FALSE\t+\/\t+FALSE\t+0\t+PHPSESSID\t+(.+)\n$/", file_get_contents($this->client->get_cookie_file_path()), $matches)) {
+			$this->client_session_id = $matches[1];
+			file_put_contents(SITEPATH . '/uploads/session_is_test_' . $this->client_session_id, '1');
+		}
 	}
 
+	/**
+	 * Clean up
+	 */
+	public function __destruct() {
+		// Remove test created file to identify this connection as a test envoirement.
+		if (!empty($this->client_session_id) && file_exists(SITEPATH . '/uploads/session_is_test_' . $this->client_session_id)) {
+			@unlink(SITEPATH . '/uploads/session_is_test_' . $this->client_session_id);
+		}
+	}
 
 	/**
 	 * Do a GET and store the content within the current content variable.
@@ -36,10 +66,22 @@ class WebUnitTest extends UnitTest {
 	 *   the url.
 	 * @param array $args
 	 *   The GET arguments (optional, default = array())
+	 * @param boolean $full_path
+	 *   If set to true the provided url will be direct executed,
+	 *   else it will prepend the base_domain
+	 *   which is http://{domain} (optional, default = false)
 	 *
 	 * @return string the body content.
 	 */
-	public function do_get($url, $args = array()) {
+	public function do_get($url, $args = array(), $full_path = false) {
+		if ($full_path === false && !empty($url) && $url{0} !== '/') {
+			$url = '/' . $url;
+		}
+
+		if ($full_path === false) {
+			$url = $this->base_domain . $url;
+		}
+
 		return $this->content = $this->client->do_get($url, $args);
 	}
 
@@ -50,10 +92,22 @@ class WebUnitTest extends UnitTest {
 	 *   the url.
 	 * @param array $args
 	 *   The GET arguments (optional, default = array())
+	 * @param boolean $full_path
+	 *   If set to true the provided url will be direct executed,
+	 *   else it will prepend the base_domain
+	 *   which is http://{domain} (optional, default = false)
 	 *
 	 * @return string the body content.
 	 */
-	public function do_post($url, $args = array()) {
+	public function do_post($url, $args = array(), $full_path = false) {
+		if ($full_path === false && !empty($url) && $url{0} !== '/') {
+			$url = '/' . $url;
+		}
+
+		if ($full_path === false) {
+			$url = $this->base_domain . $url;
+		}
+
 		return $this->content = $this->client->do_post($url, $args);
 	}
 
