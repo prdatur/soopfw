0e48f11c5e4f831241f69b73187e156476d00a29
Christian Ackermann <prdatur@gmail.com>
2012-10-07 20:50:38 +0200

    fix get_widget interface method body

--- lib/interfaces/Widget.class.php
+++ lib/interfaces/Widget.class.php
@@ -14,12 +14,18 @@ interface Widget {
 	 * Initialize the widget, will also perform form handlings for the widget if needed.
 	 * This method must perform all actions what the widget should can do.
 	 *
+	 * Use only the returned uuid to access the widget because non "word" character will be replaced
+	 * to _ (underline)
+	 *
 	 * @param string $name
 	 *   the widget name
-	 *
 	 * @param string $unique_id
 	 *   the unique id for this widget
+	 * @param Configuration $widget_config
+	 *   the widget configuration object (optional, default = null)
+	 *
+	 * @return string the cleaned uuid
 	 */
-	public function get_widget($name, $unique_id);
+	public function get_widget($name, $unique_id, Configuration $widget_config = null);
 }
 
