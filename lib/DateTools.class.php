54da63f2be4b80517d9899b0f21a0ad1a2b886d3
Christian Ackermann <prdatur@gmail.com>
2012-10-21 19:30:31 +0200

    added usefull date tools

---
+++ lib/DateTools.class.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * This class will bring usefull date tools.
+ *
+ * @copyright Christian Ackermann (c) 2010 - End of life
+ * @author Christian Ackermann <prdatur@gmail.com>
+ * @package lib
+ * @category Tools
+ */
+class DateTools
+{
+
+	/**
+	 * Define some static times.
+	 */
+	const TIME_MINUTE = 60;
+	const TIME_MINUTE_5 = 300;
+	const TIME_MINUTE_10 = 600;
+	const TIME_MINUTE_15 = 900;
+	const TIME_MINUTE_20 = 1200;
+	const TIME_MINUTE_25 = 1500;
+	const TIME_MINUTE_30 = 1800;
+	const TIME_MINUTE_35 = 2100;
+	const TIME_MINUTE_40 = 2400;
+	const TIME_MINUTE_45 = 2700;
+	const TIME_MINUTE_50 = 3000;
+	const TIME_MINUTE_55 = 3300;
+	const TIME_HOUR = 3600;
+	const TIME_DAY = 86400;
+	const TIME_31_MONTH = 2678400;
+	const TIME_30_MONTH = 2592000;
+	const TIME_28_MONTH = 2419200;
+	const TIME_YEAR = 31536000;
+
+}
+
