a5266cd00f84ee7cb66e857db5d6e947946dd658
Christian Ackermann <prdatur@gmail.com>
2012-10-30 22:57:45 +0100

    add default security exception

---
+++ lib/exceptions/SoopfwSecurityException.class.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Provides an exception to determine a security error.
+ *
+ * @copyright Christian Ackermann (c) 2010 - End of life
+ * @author Christian Ackermann <prdatur@gmail.com>
+ * @category Exceptions
+ */
+class SoopfwSecurityException extends Exception
+{
+}
