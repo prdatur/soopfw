106ba7ec182fd4a3476e2083cd90cfc6843f0b5f
Christian Ackermann <prdatur@gmail.com>
2012-11-03 13:31:24 +0100

    implemented new tests to check if all created modules and there ajax / action files are correct.

--- modules/system/tests/DefaultWebSystemTest.class.php
+++ modules/system/tests/DefaultWebSystemTest.class.php
@@ -21,8 +21,81 @@ class DefaultWebSystemTest extends WebUnitTest implements UnitTestInterface
 	}
 
 	/**
-	 * Checks main database functionality.
-	 *
+	 * Checks if all web/ajax calls are correct.
+	 */
+	public function check_module_actions() {
+		// Get all modulese.
+		$dir = new Dir('modules', false);
+		$dir->just_dirs();
+
+		foreach ($dir AS $entry) {
+
+			// Verify module info and entry file-
+			$module_name = $entry->filename;
+			$this->assert_true(file_exists($entry->path . '/' . $module_name . '.info'), t('Check for valid module info file: @module', array('@module' => $module_name)));
+			$this->assert_true(file_exists($entry->path . '/' . $module_name . '.php'), t('Check for valid module entry file: @module', array('@module' => $module_name)));
+
+			// Check if the module entry class exist.
+			$module_class = WebAction::generate_classname($module_name);
+			$this->assert_true(class_exists($module_class), t('Check if module class exist: @module', array('@module' => $module_class)));
+
+			// Verify all ajax request files.
+			$ajax_dir = new Dir('modules/' . $module_name . '/ajax', false);
+			$ajax_dir->just_files();
+			$ajax_dir->file_extension('php');
+
+			foreach ($ajax_dir AS $ajax_entry) {
+				// Remvoe the extension so we get the pure filename.
+				$action = preg_replace("/\.php$/", "", $ajax_entry->filename);
+
+				// Get the parsed class which is needed.
+				$ajax_class = WebAction::generate_classname('ajax_' . $module_name . '_' . $action);
+
+				// Check if the class exist.
+				if ($this->assert_true(class_exists($ajax_class), t('Check if ajax class exist: @ajax', array('@ajax' => $ajax_class)))) {
+
+					// Generate the ajax call class object to check if it extends the needed class.
+					$obj = new $ajax_class();
+
+					$this->assert_true(($obj instanceof AjaxModul), t('Check if ajax class extends AjaxModul: @ajax', array('@ajax' => $ajax_class)));
+
+					// Check if the run method was implemented, so an ajax call can be executed.
+					$this->assert_true(method_exists($obj, 'run'), t('Check if ajax class implements run method: @ajax', array('@ajax' => $ajax_class)));
+				}
+			}
+
+			// Validate web actions directory.
+			$web_action_dir = new Dir('modules/' . $module_name . '/actions', false);
+			$web_action_dir->just_files();
+			$web_action_dir->file_extension('php');
+
+			foreach ($web_action_dir AS $web_action_entry) {
+				// Remvoe the extension so we get the pure filename.
+				$action = preg_replace("/\.php$/", "", $web_action_entry->filename);
+
+				// Get the parsed class which is needed.
+				$web_action_class = WebAction::generate_classname($module_name . '_' . $action);
+
+				// Check if the class exist.
+				if ($this->assert_true(class_exists($web_action_class), t('Check if action class exist: @class', array('@class' => $web_action_class)))) {
+
+					// Generate the action call class object to check if it extends the needed class.
+					$obj = new $web_action_class();
+
+					$this->assert_true(($obj instanceof ActionModul), t('Check if "@class" class extends ActionModul', array('@class' => $web_action_class)));
+
+					// Check if the run method was implemented, so an action call can be executed.
+					$this->assert_true(method_exists($obj, 'run'), t('Check if "@class" class implements the "@action" method', array(
+						'@class' => $web_action_class,
+						'@action' => $web_action_entry->filename,
+					)));
+				}
+			}
+		}
+	}
+
+	/**
+	 * Check if we can log in.
 	 */
 	public function check_root() {
 		$this->do_get('/user/login.html');
