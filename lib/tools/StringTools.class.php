2ca1bda3ad1ab0c0f787476ede7c47bc91d59308
Christian Ackermann <prdatur@gmail.com>
2012-11-10 15:06:40 +0100

    implemented gzdecode

--- lib/tools/StringTools.class.php
+++ lib/tools/StringTools.class.php
@@ -9,8 +9,6 @@
  */
 class StringTools
 {
-
-
 	/**
 	 * Char save truncate policy, will break right after specified chars.
 	 *
@@ -38,7 +36,6 @@ class StringTools
 	 * @var string
 	 */
 	const TRUNCATE_POLICY_SENTENCE_SAVE = 'sentence_save';
-
 
 	/**
 	 * Truncate a string to a certain length if necessary,
@@ -106,7 +103,6 @@ class StringTools
 			else {
 				return $string;
 			}
-
 		}
 		else {
 
@@ -148,7 +144,6 @@ class StringTools
 			else {
 				return $string;
 			}
-
 		}
 	}
 
@@ -303,6 +298,131 @@ class StringTools
 		}
 		$out[] = '';
 		return join("\n", $out);
+	}
+
+	/**
+	 * Decodes a gzip compressed string.
+	 * 
+	 * @param string $data
+	 *   The data to be decoded.
+	 * @param string $filename
+	 *   Holds the filename information.
+	 *   (Internally used only)
+	 * @param string $error
+	 *   Holds the errror message if one occure.
+	 * @param int $maxlength
+	 *   The maximum data to be encoded.
+	 * @return null|boolean|string Returns null if provided $data is not a gzip format, returns boolean false if en error occured and finally returns the data string on success.
+	 */
+	public static function gzdecode($data, &$filename = '', &$error = '', $maxlength = null) {
+		$len = strlen($data);
+		if ($len < 18 || strcmp(substr($data, 0, 2), "\x1f\x8b")) {
+			$error = "Not in GZIP format.";
+			return null;  // Not GZIP format (See RFC 1952)
+		}
+		$method = ord(substr($data, 2, 1));  // Compression method
+		$flags = ord(substr($data, 3, 1));  // Flags
+		if ($flags & 31 != $flags) {
+			$error = "Reserved bits not allowed.";
+			return null;
+		}
+		// NOTE: $mtime may be negative (PHP integer limitations)
+		$mtime = unpack("V", substr($data, 4, 4));
+		$mtime = $mtime[1];
+		$xfl = substr($data, 8, 1);
+		$os = substr($data, 8, 1);
+		$headerlen = 10;
+		$extralen = 0;
+		$extra = "";
+		if ($flags & 4) {
+			// 2-byte length prefixed EXTRA data in header
+			if ($len - $headerlen - 2 < 8) {
+				return false;  // invalid
+			}
+			$extralen = unpack("v", substr($data, 8, 2));
+			$extralen = $extralen[1];
+			if ($len - $headerlen - 2 - $extralen < 8) {
+				return false;  // invalid
+			}
+			$extra = substr($data, 10, $extralen);
+			$headerlen += 2 + $extralen;
+		}
+		$filenamelen = 0;
+		$filename = "";
+		if ($flags & 8) {
+			// C-style string
+			if ($len - $headerlen - 1 < 8) {
+				return false; // invalid
+			}
+			$filenamelen = strpos(substr($data, $headerlen), chr(0));
+			if ($filenamelen === false || $len - $headerlen - $filenamelen - 1 < 8) {
+				return false; // invalid
+			}
+			$filename = substr($data, $headerlen, $filenamelen);
+			$headerlen += $filenamelen + 1;
+		}
+		$commentlen = 0;
+		$comment = "";
+		if ($flags & 16) {
+			// C-style string COMMENT data in header
+			if ($len - $headerlen - 1 < 8) {
+				return false;	// invalid
+			}
+			$commentlen = strpos(substr($data, $headerlen), chr(0));
+			if ($commentlen === false || $len - $headerlen - $commentlen - 1 < 8) {
+				return false;	// Invalid header format
+			}
+			$comment = substr($data, $headerlen, $commentlen);
+			$headerlen += $commentlen + 1;
+		}
+		$headercrc = "";
+		if ($flags & 2) {
+			// 2-bytes (lowest order) of CRC32 on header present
+			if ($len - $headerlen - 2 < 8) {
+				return false;	// invalid
+			}
+			$calccrc = crc32(substr($data, 0, $headerlen)) & 0xffff;
+			$headercrc = unpack("v", substr($data, $headerlen, 2));
+			$headercrc = $headercrc[1];
+			if ($headercrc != $calccrc) {
+				$error = "Header checksum failed.";
+				return false;	// Bad header CRC
+			}
+			$headerlen += 2;
+		}
+		// GZIP FOOTER
+		$datacrc = unpack("V", substr($data, -8, 4));
+		$datacrc = sprintf('%u', $datacrc[1] & 0xFFFFFFFF);
+		$isize = unpack("V", substr($data, -4));
+		$isize = $isize[1];
+		// decompression:
+		$bodylen = $len - $headerlen - 8;
+		if ($bodylen < 1) {
+			// IMPLEMENTATION BUG!
+			return null;
+		}
+		$body = substr($data, $headerlen, $bodylen);
+		$data = "";
+		if ($bodylen > 0) {
+			switch ($method) {
+				case 8:
+					// Currently the only supported compression method:
+					$data = gzinflate($body, $maxlength);
+					break;
+				default:
+					$error = "Unknown compression method.";
+					return false;
+			}
+		}  // zero-byte body content is allowed
+		// Verifiy CRC32
+		$crc = sprintf("%u", crc32($data));
+		$crcOK = $crc == $datacrc;
+		$lenOK = $isize == strlen($data);
+		if (!$lenOK || !$crcOK) {
+			$error = ( $lenOK ? '' : 'Length check FAILED. ') . ( $crcOK ? '' : 'Checksum FAILED.');
+			return false;
+		}
+		return $data;
 	}
 
 }
