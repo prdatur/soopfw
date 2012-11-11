e3e298db14350e51a57e9a37fe74e60b6bbab8c3
Christian Ackermann <prdatur@gmail.com>
2012-11-11 15:00:33 +0100

    re-activated the email confirmation where an admin changes a user password

--- modules/user/ajax/user_change_password.php
+++ modules/user/ajax/user_change_password.php
@@ -12,19 +12,11 @@ class AjaxUserUserChangePassword  extends AjaxModul {
 	 * This function will be executed after ajax file initializing
 	 */
 	public function run() {
-		//Check perms
-		if (!$this->core->get_right_manager()->has_perm("admin.user.change")) {
-			AjaxModul::return_code(AjaxModul::ERROR_NO_RIGHTS);
-		}
-
 		//Setup needed params
 		$params = new ParamStruct();
 		$params->add_required_param("user_id", PDT_INT);
 		$params->add_required_param("password", PDT_STRING);
-		/**
-		 * DISABLED UNTIL EMAIL TEMPLATE CONFIGURATION IS IMPLEMENTED
-		 */
-		// $params->add_isset_param("inform", PDT_BOOL, false);
+		$params->add_param("inform", PDT_BOOL, false);
 		$params->fill();
 
 		//Check params
@@ -32,22 +24,27 @@ class AjaxUserUserChangePassword  extends AjaxModul {
 			AjaxModul::return_code(AjaxModul::ERROR_MISSING_PARAMETER);
 		}
 
+		//Check perms (perm admin.user.change is allowed and the current user can change his own password)
+		if ($this->session->current_user()->user_id != $params->user_id && !$this->core->get_right_manager()->has_perm("admin.user.change")) {
+			AjaxModul::return_code(AjaxModul::ERROR_NO_RIGHTS);
+		}
+
 		//Load the user, set the new password and save it
 		$user_obj = new UserObj($params->user_id);
 		$user_obj->password = $params->password;
 
 		if ($user_obj->save(true, true)) {
+			if ($this->session->current_user()->user_id != $params->user_id) {
+				// Inform the user if we wanted it.
+				if ($params->inform == true && $this->core->get_right_manager()->has_perm("admin.user.change")) {
+					$tpl_vals['password'] = $params->password;
+					$tpl_vals['username'] = $user_obj->username;
+					$mail = new Email();
+					$mail->send_tpl( User::CONFIG_MAIL_TEMPLATE_CHANGE_PASSWORD, $user_obj->language, $user_obj->get_address_by_group(UserAddressObj::USER_ADDRESS_GROUP_DEFAULT, "email"), $tpl_vals);
+				}
 
-			//If we wanted to inform the user about the new password, send an email
-
-			/**
-			 * DISABLED UNTIL EMAIL TEMPLATE CONFIGURATION IS IMPLEMENTED
-			 */
-			if (false && $params->inform == true) {
-				$tpl_vals['password'] = $params->password;
-				$tpl_vals['username'] = $user_obj->username;
-				$mail = new Email();
-				$mail->send_tpl("admin_edit_user_password", $user_obj->language, $user_obj->get_address_by_group(UserAddressObj::USER_ADDRESS_GROUP_DEFAULT, "email"), $tpl_vals);
+				// Audit only if we have not changed our own password.
+				SystemHelper::audit(t('Changed password for user "@username"', array('@username' => $user_obj->username)), 'user');
 			}
 			AjaxModul::return_code(AjaxModul::SUCCESS);
 		}
