1dc02a33c3a955144d85e87c29b79f7bad2a7dc7
Christian Ackermann <prdatur@gmail.com>
2012-11-18 14:43:49 +0100

    moved NetTools to tools directory

--- lib/tools/NetTools.class.php
+++ lib/tools/NetTools.class.php
@@ -44,7 +44,7 @@ class NetTools
 	 * @param string $value
 	 *   the email
 	 * @param boolean $check_dns
-	 *   wether to check against the dns record for valid mx record or not
+	 *   whether to check against the dns record for valid mx record or not
 	 * 	 (optional, default = true)
 	 *
 	 * @return boolean true if valid, else false
@@ -67,7 +67,7 @@ class NetTools
 	}
 
 	/**
-	 * Returns wether the current connection is a local ip or not.
+	 * Returns whether the current connection is a local ip or not.
 	 *
 	 * @return boolean true if current ip is a local ip or false if not
 	 */
@@ -103,15 +103,22 @@ class NetTools
 	public static function get_real_ip($ip2long = false) {
 		static $ip = null;
 
+		// Only read the ip one time.
 		if ($ip === null) {
+
+			// Check if we have a proxy.
 			if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+
+				// Get the direct remote address which is the proxy ip.
 				$client_ip = (!empty($_SERVER['REMOTE_ADDR']) ) ? $_SERVER['REMOTE_ADDR'] : ( (!empty($_ENV['REMOTE_ADDR']) ) ? $_ENV['REMOTE_ADDR'] : "unknown" );
 
+				// Get all forwarded entries.
 				$entries = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
 
 				reset($entries);
 				while (list(, $entry) = each($entries)) {
 					$entry = trim($entry);
+					// Check the forwarded entry if they are valid ips.
 					if (preg_match("/^([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)/", $entry, $ip_list)) {
 						$private_ip = array(
 							'/^0\./',
@@ -120,8 +127,10 @@ class NetTools
 							'/^172\.((1[6-9])|(2[0-9])|(3[0-1]))\..*/',
 							'/^10\..*/');
 
+						// Remove all private ips.
 						$found_ip = preg_replace($private_ip, $client_ip, $ip_list[1]);
 
+						// If the current forwarded ip is not a private one and not the proxy ip it is the real remote address.
 						if ($client_ip != $found_ip) {
 							$client_ip = $found_ip;
 							break;
@@ -130,21 +139,79 @@ class NetTools
 				}
 			}
 			else {
+				// We have no proxy so get the direct remote address.
 				$client_ip = (!empty($_SERVER['REMOTE_ADDR']) ) ? $_SERVER['REMOTE_ADDR'] : ( (!empty($_ENV['REMOTE_ADDR']) ) ? $_ENV['REMOTE_ADDR'] : "unknown" );
 			}
+			// If we have a ipv6 convertion get only the ip4 string.
 			if (strpos($client_ip, '::') === 0) {
 				$client_ip = substr($client_ip, strrpos($client_ip, ':') + 1);
 			}
+
 			$long = ip2long($client_ip);
 			if (!$long) {
 				$long = 0;
 			}
 
+			// Cache the data.
 			$ip[false] = $client_ip;
 			$ip[true] = $long;
 		}
 
+		// Return the wanted ip.
 		return $ip[$ip2long];
+	}
+
+	/**
+	 * Download the given content as the given extension.
+	 *
+	 * @param string $content
+	 *   The complete content.
+	 * @param string $filename
+	 *   The complete filename INCLUDING the extension.
+	 * @param string $extension
+	 *   The extension, please provide NOT a mimetype.
+	 *   For example: 'xml', 'tar.gz'...
+	 * @param int $expires
+	 *   The expire time which will be set within the headers.
+	 *   The value are seconds and are NOT the date instead the value will be added to the current date.
+	 *    (optional, default = DateTools::TIME_DAY)
+	 * @param int $last_modofied
+	 *   The last modification time.
+	 *   The value is the direct date in seconds. (optional, default = TIME_NOW)
+	 */
+	public static function download($content, $filename, $extension, $expires = DateTools::TIME_DAY, $last_modified = TIME_NOW) {
+
+		// Holds all extensions which should be downloaded as "text/plain"
+		$plain_text = array(
+			'txt' => '',
+		);
+
+		//Set http headers
+		header("Expires: " . gmdate("D, d M Y H:i:s", TIME_NOW + $expires) . " GMT+1");
+		header("Last-Modified: " . gmdate("D, d M Y H:i:s", $last_modified) . " GMT+1");
+		header("Content-Length: " . strlen($content));
+
+		// If we have not a text/plain extension.
+		if (!isset($plain_text[$extension])) {
+			header("Content-Transfer-Encoding: binary");
+
+			// Default mime type if we have not found a valid mime type for the given extension.
+			$content_type = "application/" . $extension;
+
+			// Try get the mime type for the extension.
+			$obj = new MimeTypeObj($extension);
+			if ($obj->load_success()) {
+				$content_type = $obj->mime_type;
+			}
+
+			header("Content-type: " . $content_type . "\n");
+			header("Content-Disposition: attachment; filename=\"" . $filename . "\";\n\n");
+		}
+		else {
+			header("Content-type: text/plain\n");
+		}
+		echo $content;
+		die();
 	}
 
 }
